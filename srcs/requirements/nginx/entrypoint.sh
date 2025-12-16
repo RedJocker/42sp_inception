457f9871c856b368d59cf02496ae25153566c2a1
redjocker <mauricio.guaruja@gmail.com>
2025-12-16 02:41:12 -0300

    extract wordpress network alias

--- srcs/requirements/nginx/entrypoint.sh
+++ srcs/requirements/nginx/entrypoint.sh
@@ -3,6 +3,7 @@
 envsubst '${SSL_CERT_PATH}  \
 	 ${SSL_CERT_KEY_PATH}  \
 	 ${WORDPRESS_PORT} \
+	 ${WORDPRESS_NETWORK_ALIAS} \
 	 ${NGINX_CONTAINER_PORT}'  \
     < /etc/nginx/nginx.conf.template \
     > /etc/nginx/nginx.conf
